93c1bd1dd13a751de192a95efebf017a6e3919ba
Greg <gregory.w.young@gmail.com>
2013-03-12 18:47:29 -0700

    Adding basic tastypie API

---
+++ viewer/api.py
@@ -0,0 +1,19 @@
+from tastypie import fields
+from tastypie.resources import ModelResource, ALL
+from viewer.models import Piece, Link
+
+
+class PieceResource(ModelResource):
+    class Meta:
+        queryset = Piece.objects.all()
+        filtering = {
+            'type': ALL,
+            'created_date': ALL,
+        }
+
+class LinkResource(ModelResource):
+    class Meta:
+        queryset = Link.objects.all()
+        filtering = {
+            'type': ALL,
+        }
