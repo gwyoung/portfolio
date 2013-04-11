c5a58108cf45bff64be57050a940290a5e2b3dd3
Greg <gregory.w.young@gmail.com>
2013-04-10 19:27:03 -0700

    Fixed up minor issues and added favicon

--- viewer/static/js/models/piece.js
+++ viewer/static/js/models/piece.js
@@ -8,7 +8,7 @@ define([
     var Piece = Backbone.Model.extend({
         urlRoot: Server.Constants.PIECE_API,
         isHorizontal: function (){
-            return parseInt(this.get('width')) >= parseInt(this.get('height'));
+            return parseFloat(this.get('width')) >= parseFloat(this.get('height'));
         }
     });
 
