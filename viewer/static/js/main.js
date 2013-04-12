87c8dd0c15aa18d543ea6be637470a9c51732466
Greg <gregory.w.young@gmail.com>
2013-04-11 22:35:09 -0700

    Merging xhr change to main.js and re-adding git-ignore

--- viewer/static/js/main.js
+++ viewer/static/js/main.js
@@ -18,6 +18,15 @@ require.config({
             'deps': ['backbone'],
             'exports': 'Tastypie'
         }
+    },
+    config: {
+        text: {
+            useXhr: function (url, protocol, hostname, port) {
+                // allow cross-domain requests
+                // remote server allows CORS
+                return true;
+            }
+        }
     }
 });
 
